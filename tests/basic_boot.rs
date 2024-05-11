faf4adb0b484f7d6fcce56cea679d84c2a7b222c
Hu Bowen <contact@hubowen.dev>
2024-05-11 21:15:59 +0800

    feat: added unit tests

--- tests/basic_boot.rs
+++ tests/basic_boot.rs
@@ -4,30 +4,22 @@
 #![test_runner(rust_os::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
-mod serial;
-mod vga_buffer;
-
 use core::panic::PanicInfo;
+
+use rust_os::println;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    println!("Hello from _start!");
-    println!("Here are some numbers: {}, {}", 1337, 0.1 + 0.2);
-
-    #[cfg(test)]
     test_main();
 
     loop {}
 }
 
-#[cfg(not(test))]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
-    loop {}
+#[test_case]
+fn test_println() {
+    println!("test_println output");
 }
 
-#[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     rust_os::test_panic_handler(info)
